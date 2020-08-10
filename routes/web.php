147dd9e765d9b9290b6d8c2c5170b187133771b3
Armin Abbasi <abbasi@netbina.com>
2020-08-10 18:53:02 +0430

    Redefined authentication routes

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['prefix' => 'auth'], function () {
+Route::get('/', function() {
+    return redirect()->route('main');
+});
+
+Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function () {
     Route::get('login', 'UserController@login')->name('login');
+    Route::post('logout', 'LoginController@logout ')->name('logout');
     Route::post('sign-in', 'UserController@signIn')->name('sign-in');
     Route::get('register', 'UserController@register')->name('register');
     Route::post('sign-up', 'UserController@signUp')->name('sign-up');
+    Route::post('resend', 'VerificationController@resend')->name('verification.resend');
+    Route::get('verify', 'VerificationController@show')->name('verification.notice');
+    Route::get('verify/{id}/{hash}', 'VerificationController@verify')->name('verification.verify');
 });
 
-Route::group(['middleware' => 'auth'], function () {
-    Route::get('/', 'UserController@index');
+Route::group(['prefix' => 'dashboard', 'middleware' => ['auth', 'verified']], function () {
+    Route::get('/', 'DashboardController@index')->name('main');
 });
+
+//Auth::routes(['verify' => true]);
