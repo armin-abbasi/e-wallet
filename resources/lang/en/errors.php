1a9ec43f8b454616e8c3ca170722ac9a77b60059
Armin Abbasi <armin.abbasi@hotmail.com>
2020-08-11 23:08:06 +0430

    Added new lang files

---
+++ resources/lang/en/errors.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'invalid_user'  => 'User ID does not match the wallet owner.',
+    'delete_failed' => 'Failed to delete this item',
+];
---
+++ resources/lang/en/messages.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'no_wallets'  => 'You don\'t have any wallets, you can create one by clicking New Wallet button.',
+    'no_invoices' => 'There are no invoice records in this wallet.',
+];
